75709a2de4a0e54b51d33530a90e04fb296ac1b4
riyanandri <hiddenfoxe01@gmail.com>
2022-07-06 15:17:12 +0700

    final changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:aas_1905040046/View/Screens/Home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: BelajarGetData(),
-  ));
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
 }
 
-class BelajarGetData extends StatelessWidget {
-  final String apiUrl = "https://reqres.in/api/users?per_page=15";
-  Future<List<dynamic>> _fecthDataUsers() async {
-    var result = await http.get(Uri.parse(apiUrl));
-    return json.decode(result.body)['data'];
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Belajar GET HTTP'),
-      ),
-      body: Container(
-        child: FutureBuilder<List<dynamic>>(
-          future: _fecthDataUsers(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                padding: EdgeInsets.all(10),
-                itemCount: snapshot.data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      backgroundImage: 
-                        NetworkImage(snapshot.data[index]['avatar']),
-                    ),
-                    title: Text(snapshot.data[index]['first_name'] + "" + snapshot.data[index]['last_name']),
-                    subtitle: Text(snapshot.data[index]['email']),
-                  );
-                });
-            }else {
-              return Center(child: CircularProgressIndicator());
-            }
-          },
-        ),
-      ),
-    );    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
   }
 }
+
